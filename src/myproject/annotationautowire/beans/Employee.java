3f569ba87c93c8b7729d18fed51036506df6aca8
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2022-01-12 15:10:33 +0530

    Annotation using constructor

--- src/myproject/annotationautowire/beans/Employee.java
+++ src/myproject/annotationautowire/beans/Employee.java
@@ -10,56 +10,66 @@ public class Employee {
 	private String ename;
 	private float esal;
 	private String eaddr;
+
+	private Account acc;
+
 //	@Required
 	@Autowired(required = true)
-	@Qualifier("currentAcc")
-	private Account acc ;
-	public String getEid() {
-		return eid;
-	}
-	public void setEid(String eid) {
+	
+	public Employee(String eid, String ename, float esal, String eaddr, @Qualifier("currentAcc")Account acc) {
 		this.eid = eid;
-	}
-	public String getEname() {
-		return ename;
-	}
-	public void setEname(String ename) {
 		this.ename = ename;
-	}
-	public float getEsal() {
-		return esal;
-	}
-	public void setEsal(float esal) {
 		this.esal = esal;
-	}
-	public String getEaddr() {
-		return eaddr;
-	}
-	public void setEaddr(String eaddr) {
 		this.eaddr = eaddr;
-	}
-	public Account getAcc() {
-		return acc;
-	}
-	
-	
-	public void setAcc(Account acc) {
 		this.acc = acc;
 	}
-	
+
+//	public String getEid() {
+//		return eid;
+//	}
+//	public void setEid(String eid) {
+//		this.eid = eid;
+//	}
+//	public String getEname() {
+//		return ename;
+//	}
+//	public void setEname(String ename) {
+//		this.ename = ename;
+//	}
+//	public float getEsal() {
+//		return esal;
+//	}
+//	public void setEsal(float esal) {
+//		this.esal = esal;
+//	}
+//	public String getEaddr() {
+//		return eaddr;
+//	}
+//	public void setEaddr(String eaddr) {
+//		this.eaddr = eaddr;
+//	}
+//	public Account getAcc() {
+//		return acc;
+//	}
+//	
+//	
+//	public void setAcc(Account acc) {
+//		this.acc = acc;
+//	}
+
 	public void getEmpDetails() {
 		System.out.println("Employee Details");
 		System.out.println("------------------------------");
-		System.out.println("Employee Id  	: "+eid);
-		System.out.println("Employee name 	: "+ename);
-		System.out.println("Employee Salary : "+esal);
-		System.out.println("Employee Address : "+eaddr);
+		System.out.println("Employee Id  	: " + eid);
+		System.out.println("Employee name 	: " + ename);
+		System.out.println("Employee Salary : " + esal);
+		System.out.println("Employee Address : " + eaddr);
 		System.out.println();
 		System.out.println("Account Details");
 		System.out.println("---------------------------------");
-		System.out.println("Account Number  : "+acc.getAccNo());
-		System.out.println("Account Name    : "+acc.getAccName());
-		System.out.println("Account Type    : "+acc.getAccType());
-		System.out.println("Account Balance : "+acc.getBalance());
+		System.out.println("Account Number  : " + acc.getAccNo());
+		System.out.println("Account Name    : " + acc.getAccName());
+		System.out.println("Account Type    : " + acc.getAccType());
+		System.out.println("Account Balance : " + acc.getBalance());
 	}
 }
